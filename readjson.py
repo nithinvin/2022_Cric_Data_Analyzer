a847beed1b680e8c49daeba261862a508aac907e
Nithin V <nithinvblr@gmail.com>
2022-03-21 18:53:45 +0530

    key,value

--- readjson.py
+++ readjson.py
@@ -2,7 +2,8 @@ import json
 f = open("F:\\Nithin_cricdata\\JSON\\2021\\1254117.json", "r")
 
 data = json.load(f)
-for i in data["info"]["outcome"]:
-    print(i)
+outcome_data = data["info"]["outcome"]
+for key, value in outcome_data.items():
+    print(key, ":", value)
 
 f.close()
