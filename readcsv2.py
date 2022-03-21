edd8df34c1d8fab3098acfa7c2665aa6b05fdff9
Nithin V <nithinvblr@gmail.com>
2022-03-21 18:03:23 +0530

    Print striker name, runs scored and bowler name

--- readcsv2.py
+++ readcsv2.py
@@ -1,19 +1,37 @@
-file_handle = open("F:\\Nithin_cricdata\csv2\ipl_male_csv2\\1254117.csv", "r")
 ball_no = 15
+striker_fieldno = 9
+bowler_fieldno = 11
+runs_of_bat_fieldno = 12
+extras_fieldno = 13
+
+file_handle = open("F:\\Nithin_cricdata\csv2\ipl_male_csv2\\1254117.csv", "r")
 Lines = file_handle.readlines()
+
 count = 0
 striker_name = ""
+bowler_name = ""
+runs_of_bat = ""
+extras = ""
+
 for line in Lines:
     count += 1
     if count == ball_no + 1:
         field_count = 0
         for char in line:
             if (char == ','):
-                field_count += 1
-                if field_count == 9:
-                    break
+                field_count += 1                
             else:
-                if field_count == 8:
+                if field_count == striker_fieldno - 1:
                     striker_name += char
+                if field_count == bowler_fieldno - 1:
+                    bowler_name += char
+                if field_count == runs_of_bat_fieldno - 1:
+                    runs_of_bat += char
+                if field_count == extras_fieldno - 1:
+                    extras += char
         #print("Line{}: {}".format(count, line.strip()))
-        print ("Striker is", striker_name)
+        if runs_of_bat:
+            print("Striker", striker_name, "scored", runs_of_bat, "when", bowler_name, "bowled")
+        else:
+            print("Striker", striker_name, "faced", bowler_name, "while extras", extras, "were scored")
+        
