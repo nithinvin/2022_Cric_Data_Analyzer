4fc7b7081a7fc6d767df610bde38cdbbb220781d
Nithin V <nithinvblr@gmail.com>
2022-03-23 19:54:36 +0530

    both innings batter scorecard

--- printplayer.py
+++ printplayer.py
@@ -1,23 +1,30 @@
-import json
-f = open("F:\\Nithin_cricdata\\JSON\\2021\\1254117.json", "r")
+def print_batters_scoreboard(overs_data):
+    batter_scoreboard = {}    
+    for overs in overs_data:
+        for key, value in overs.items():
+            if key == "deliveries":
+                for delivery in value:
+                    if delivery["batter"] not in batter_scoreboard.keys():
+                        batter_scoreboard[delivery["batter"]] = 0
 
-data = json.load(f)
+                    batter_scoreboard[delivery["batter"]] += delivery["runs"]["batter"]
+                        #deliveries_faced += 1
 
-batter_scoreboard = {}
+    #print("Runs scored by", batter_to_print, "is", runs_by_batter, "in", deliveries_faced, "balls")
+    for batter, runs in batter_scoreboard.items():
+        print("Runs scored by", batter, "is", runs)
 
-overs_data = data["innings"][1]["overs"]
-for overs in overs_data:
-    for key, value in overs.items():
-        if key == "deliveries":
-            for delivery in value:
-                if delivery["batter"] not in batter_scoreboard.keys():
-                    batter_scoreboard[delivery["batter"]] = 0
 
-                batter_scoreboard[delivery["batter"]] += delivery["runs"]["batter"]                
-                    #deliveries_faced += 1
+def main():
+    import json
+    f = open("F:\\Nithin_cricdata\\JSON\\2021\\1254117.json", "r")
+    data = json.load(f)
 
-#print("Runs scored by", batter_to_print, "is", runs_by_batter, "in", deliveries_faced, "balls")
-for batter, runs in batter_scoreboard.items():
-    print("Runs scored by", batter, "is", runs)
+    overs_data = data["innings"][0]["overs"]
+    print_batters_scoreboard(overs_data)
+    overs_data = data["innings"][1]["overs"]
+    print_batters_scoreboard(overs_data)
 
-f.close()
+    f.close()
+
+main()
