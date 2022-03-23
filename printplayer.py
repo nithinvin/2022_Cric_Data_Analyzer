f2d47ed440385e0e072beae6176c087c6fd309c2
Nithin V <nithinvblr@gmail.com>
2022-03-23 19:42:13 +0530

    batter scoreboard

--- printplayer.py
+++ printplayer.py
@@ -3,26 +3,21 @@ f = open("F:\\Nithin_cricdata\\JSON\\2021\\1254117.json", "r")
 
 data = json.load(f)
 
-batter_to_print = "F du Plessis"
-runs_by_batter = 0
-deliveries_faced = 0
+batter_scoreboard = {}
 
-overs_data = data["innings"][0]["overs"]
+overs_data = data["innings"][1]["overs"]
 for overs in overs_data:
     for key, value in overs.items():
         if key == "deliveries":
             for delivery in value:
-                #print(delivery)
-                #print(delivery["batter"])
-                #print(delivery["runs"]["batter"])
-                if delivery["batter"] == batter_to_print:
-                    runs_by_batter += delivery["runs"]["batter"]
-                    deliveries_faced += 1
+                if delivery["batter"] not in batter_scoreboard.keys():
+                    batter_scoreboard[delivery["batter"]] = 0
 
-print("Runs scored by", batter_to_print, "is", runs_by_batter, "in", deliveries_faced, "balls")
-    #print(type(overs["deliveries"]))
+                batter_scoreboard[delivery["batter"]] += delivery["runs"]["batter"]                
+                    #deliveries_faced += 1
 
-    #print(overs)
-        
+#print("Runs scored by", batter_to_print, "is", runs_by_batter, "in", deliveries_faced, "balls")
+for batter, runs in batter_scoreboard.items():
+    print("Runs scored by", batter, "is", runs)
 
 f.close()
