772945d97e6946e29b9677e8d2032e22cbfd3ea7
Nithin V <nithinvblr@gmail.com>
2022-03-24 20:47:47 +0530

    sixes, fours, deliveries and strike rate

--- printplayer.py
+++ printplayer.py
@@ -4,9 +4,15 @@ def update_batters_scoreboard(overs_data, batter_scoreboard):
             if key == "deliveries":
                 for delivery in value:
                     if delivery["batter"] not in batter_scoreboard.keys():
-                        batter_scoreboard[delivery["batter"]] = 0
+                        batter_scoreboard[delivery["batter"]] = {"runs": 0, "sixes": 0, "fours": 0, "deliveries": 0}
 
-                    batter_scoreboard[delivery["batter"]] += delivery["runs"]["batter"]
+                    batter_scoreboard[delivery["batter"]]["runs"] += delivery["runs"]["batter"]
+                    if delivery["runs"]["batter"] == 6:
+                        batter_scoreboard[delivery["batter"]]["sixes"] += 1
+                    if delivery["runs"]["batter"] == 4:
+                        batter_scoreboard[delivery["batter"]]["fours"] += 1
+                    batter_scoreboard[delivery["batter"]]["deliveries"] += 1
+
                         #deliveries_faced += 1
 
     #print("Runs scored by", batter_to_print, "is", runs_by_batter, "in", deliveries_faced, "balls")  
@@ -28,11 +34,16 @@ def main():
 
         f.close()
 
-    sorted_scoreboard = sorted([(runs, batter) for (batter, runs) in batter_scoreboard.items()], reverse = True)
+    sorted_scoreboard = sorted([(runs_data["runs"], batter) for (batter, runs_data) in batter_scoreboard.items()], reverse = True)
     orange_cap_holder = {}    
     for batter_data in sorted_scoreboard:
-        print("Runs scored by", batter_data[1], "is", batter_data[0])
-    
-    print("\n", sorted_scoreboard[0][1], "is holding the orange cap by scoring", sorted_scoreboard[0][0], "runs, when", len(sorted_scoreboard), "batters played in the tournament")
+        total_runs = batter_scoreboard[batter_data[1]]["runs"]
+        total_balls = batter_scoreboard[batter_data[1]]["deliveries"]
+        total_sixes = batter_scoreboard[batter_data[1]]["sixes"]
+        total_fours = batter_scoreboard[batter_data[1]]["fours"]
+        strike_rate = round(total_runs / total_balls * 100, 2)
+        print("Runs scored by", batter_data[1], "is", total_runs , "(" + str(total_balls) + ")", "at SR " + str(strike_rate) + ", sixes - " + str(total_sixes) + ", fours -", total_fours)
+        
+    print("\n", sorted_scoreboard[0][1], "is holding the orange cap by scoring", batter_scoreboard[batter_data[1]]["runs"], "runs, when", len(sorted_scoreboard), "batters played in the tournament")
 
 main()
